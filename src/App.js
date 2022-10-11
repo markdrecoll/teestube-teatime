bf4b881c6ab2056df7c1052a6228de3bab16e0b8
mdre797 <markdrecoll@gmail.com>
2022-10-11 17:12:22 -0500

    Added more tea data.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
 import BlackTea from './pages/TeaVarieties/BlackTea';
-import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
+// import i18n from "i18next";
+// import { useTranslation, initReactI18next } from "react-i18next";
 
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    lng: "en", // if you're using a language detector, do not define the lng option
-    fallbackLng: "en",
+// i18n
+//   .use(initReactI18next) // passes i18n down to react-i18next
+//   .init({
+//     lng: "en", // if you're using a language detector, do not define the lng option
+//     fallbackLng: "en",
 
-    interpolation: {
-      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
-    },
-    // the translations
-    // (tip move them in a JSON file and import them,
-    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    resources: {
-      en: {
-        translation: {
-          "language":"English"
-        }
-      },
-      de: {
-        translation: {
-          "language": "German"
-        }
-      }
-    },
+//     interpolation: {
+//       escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+//     },
+//     // the translations
+//     // (tip move them in a JSON file and import them,
+//     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+//     resources: {
+//       en: {
+//         translation: {
+//           "language":"English"
+//         }
+//       },
+//       de: {
+//         translation: {
+//           "language": "German"
+//         }
+//       }
+//     },
     
-  });
+//   });
 
   const lngs = {
     en: { nativeName: 'English' },
@@ -42,7 +43,7 @@ i18n
   };
 
 function App() {
-  const { t, i18n } = useTranslation();
+  // const { t, i18n } = useTranslation();
   return (
     <Router>
       <Routes>
