c64c3b86d6a319ebfc63d68887280b80bead3f1a
Mark Drecoll <markdrecoll@gmail.com>
2022-10-12 23:24:07 -0500

    Setup environmental variable. Added google map. Added more tea data.

---
+++ src/components/MapDisplay.js
@@ -0,0 +1,30 @@
+import GoogleMapReact from 'google-map-react';
+
+const MapDisplay = ({ text }) => <div>{text}</div>;
+
+export default function SimpleMap(){
+  const defaultProps = {
+    center: {
+      lat: 50.00242350556172, 
+      lng: 8.272054595464294
+    },
+    zoom: 15
+  };
+
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: '500px', width: '500px' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLEAPIKEY }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <MapDisplay
+          lat={50.00242350556172}
+          lng={8.272054595464294}
+          text="Teestube"
+        />
+      </GoogleMapReact>
+    </div>
+  );
+}
