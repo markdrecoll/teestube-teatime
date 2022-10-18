9b0ecb29ca643304ea6cede57229f52408d600c9
Mark Drecoll <markdrecoll@gmail.com>
2022-10-18 10:18:41 -0500

    Added more green teas.

--- src/pages/TeaVarieties/GreenTea.js
+++ src/pages/TeaVarieties/GreenTea.js
@@ -5,24 +5,177 @@ import Menu from '../../components/Menu';
 import { Table, Input, Button, Space, Row, Col } from "antd";
 import Highlighter from 'react-highlight-words';
 import { SearchOutlined } from '@ant-design/icons';
-// import  from '../../teaData/'
+import greenTeaData from '../../teaData/greenTeaData'
 
 const GreenTea = () => {
-    return (
-      <div>
-        <Menu />
-          <div className="col-lg-8 col-md-10 offset-lg-2 offset-md-1 col-12 shadow p-3 mb-5 mt-4 bg-white rounded">
-                <Table
-                // dataSource={}
-                // columns={columns}
-                // rowKey={}
-                pagination={{ pageSize: 10 }}
-                size="small"
-                bordered="true"
-                />
-            </div>
-      </div>
-    );
+
+  const [searchState, setSearchState] = useState({ searchText: '', searchedColumn: '' })
+  const searchInput = useRef(null);
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchState({
+      searchText: selectedKeys[0],
+      searchedColumn: dataIndex,
+    });
   };
-    
-  export default GreenTea;
+
+  const handleReset = (clearFilters, confirm) => {
+    clearFilters();
+    setSearchState({ searchText: '' });
+    confirm();
+  };
+
+  // adapted from antd table search, as well as stack overflow showing how to convert to functional https://stackoverflow.com/questions/69356379/how-to-do-column-level-search-in-ant-design-using-function-component
+  const getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={searchInput}
+          placeholder={t("Search") + " Name"}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 100 }}
+          >
+            {t("Search")}
+          </Button>
+          <Button onClick={() => handleReset(clearFilters, confirm)} size="small" style={{ width: 100 }}>
+            {t("Reset")}
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase())
+        : '',
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => searchInput && searchInput.current && searchInput.current.select());
+      }
+    },
+    render: text =>
+      searchState.searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchState.searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const columns = [
+    {
+      title: 'Tea Type',
+      dataIndex: 'type',
+      key: 'type',
+      filters: [
+        {
+          text: 'Japan Grüntee',
+          value: 'Japan Grüntee'
+        },
+        {
+          text: 'China Grüntee',
+          value: 'China Grüntee'
+        },
+        {
+          text: 'Indien Himalaya',
+          value: 'Indien Himalaya'
+        },
+        {
+          text: 'Ceylon',
+          value: 'Ceylon'
+        },
+        {
+          text: 'Verschiedene Länder',
+          value: 'Verschiedene Länder'
+        },
+        {
+          text: 'Afrika',
+          value: 'Afrika'
+        },
+        {
+          text: 'Russland',
+          value: 'Russland'
+        },
+        {
+          text: 'England (Hauptbestandteil: Ceylon)',
+          value: 'England (Hauptbestandteil: Ceylon)'
+        },
+        {
+          text: 'Ostfriesland (enthält zu 90% Assam-Tee)',
+          value: 'Ostfriesland (enthält zu 90% Assam-Tee)'
+        },
+        {
+          text: 'Infree Tee',
+          value: 'Infree Tee'
+        },
+        {
+          text: 'China Schwarz-Tee',
+          value: 'China Schwarz-Tee'
+        }
+      ],
+      onFilter: (value, record) => record.type.indexOf(value) === 0,
+    },
+    {
+      title: 'Tea Name',
+      dataIndex: 'name',
+      key: 'name',
+      ...getColumnSearchProps('name'),
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description'
+    },
+    {
+      title: '50 Grams',
+      dataIndex: 'price_50g',
+      key: 'price_50g',
+      sorter: (a, b) => a.price_50g - b.price_50g
+    },
+    {
+      title: '100 Grams',
+      dataIndex: 'price_100g',
+      key: 'price_100g',
+      sorter: (a, b) => a.price_100g - b.price_100g
+    },
+    {
+      title: '250 Grams',
+      dataIndex: 'price_250g',
+      key: 'price_250g',
+      sorter: (a, b) => a.price_250g - b.price_250g
+    }
+  ]
+
+  return (
+    <div>
+      <Menu />
+      <div className="col-lg-8 col-md-10 offset-lg-2 offset-md-1 col-12 shadow p-3 mb-5 mt-4 bg-white rounded">
+        <Table
+          dataSource={greenTeaData}
+          columns={columns}
+          rowKey={greenTeaData.key}
+          pagination={{ pageSize: 10 }}
+          size="small"
+          bordered="true"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default GreenTea;
---
+++ src/teaData/greenTeaData.js
@@ -0,0 +1,258 @@
+const greenTeaData = [
+    {
+        key: '1',
+        name: 'Japan Sencha BIO',
+        price_50g: 5.20,
+        price_100g: 9.90,
+        price_250g: 24.50,
+        type: 'Japan Grüntee',
+        description: 'Grüner, großblättriger Japan-Tee, Spitzenqualität'
+    },
+    {
+        key: '2',
+        name: 'Japan Genmaicha',
+        price_50g: 3.05,
+        price_100g: 5.45,
+        price_250g: 13.60,
+        type: 'Japan Grüntee',
+        // description: ''
+    },
+    {
+        key: '3',
+        name: 'Japan Kukicha BIO',
+        price_50g: 5.70,
+        price_100g: 10.90,
+        price_250g: 27.15,
+        type: 'Japan Grüntee',
+        description: 'Eine typische Japanische Tee-Spezialität, langes, flaches, hell- und dunkelgrüne Blatt, wenig teein'
+    },
+    {
+        key: '4',
+        name: 'Bancha (Japan type)',
+        price_50g: 3.10,
+        price_100g: 5.60,
+        price_250g: 13.75,
+        type: 'Japan Grüntee',
+        // description: ''
+    },
+    // {
+    //     key: '5',
+    //     name: 'Matcha BIO',
+    //     price_50g: ,
+    //     price_100g: ,
+    //     price_250g: ,
+    //     type: 'Japan Grüntee',
+    //     description: 'Japan Original Premium'
+    //     original text was 30 gr in Dose 39,00
+    // },
+    {
+        key: '6',
+        name: 'Chun Mee',
+        price_50g: 2.25,
+        price_100g: 4.00,
+        price_250g: 9.75,
+        type: 'China Grüntee',
+        description: 'Fein-herber Geschmack, gelblicher Aufgu0, besonders anregend'
+    },
+    {
+        key: '7',
+        name: 'Gunpowder',
+        price_50g: 2.70,
+        price_100g: 4.85,
+        price_250g: 11.85,
+        type: 'China Grüntee',
+        description: 'Kugelförmig eingerolltes Blatt, heller Aufguß, sehr anregend, mehrmals aufgießen'
+    },
+    {
+        key: '8',
+        name: 'Tempel of Heaven, Bio',
+        price_50g: 3.00,
+        price_100g: 5.45,
+        price_250g: 13.35,
+        type: 'China Grüntee',
+        description: 'Berühmt als der Beste aller grünen Tees, sehr anregend, mehrmals Aufgießen'
+    },
+    // {
+    //     key: '9',
+    //     name: 'Pu Erh Tee grün (gepreßter Pu Erh Tee)',
+    //     price_50g: ,
+    //     price_100g: ,
+    //     price_250g: ,
+    //     type: 'China Grüntee',
+    //     description: 'zzz'
+    //     original text was Stück 1,30
+    // },
+    {
+        key: '10',
+        name: 'Greenfield light teein frei',
+        price_50g: 3.70,
+        price_100g: 6.90,
+        price_250g: 17.00,
+        type: 'China Grüntee',
+        description: 'Sympathischer China Sencha, reine Tasse, angenehmer Geschmack'
+    },
+    {
+        key: '11',
+        name: 'China Grün Yunnan',
+        price_50g: 3.25,
+        price_100g: 6.00,
+        price_250g: 14.75,
+        type: 'China Grüntee',
+        // description: ''
+    },
+    {
+        key: '12',
+        name: 'Gyokuro Hikari(Japan type)',
+        price_50g: 13.50,
+        price_100g: 26.30,
+        price_250g: 65.75,
+        type: 'China Grüntee',
+        description: 'Der erlesenste Sencha, im Schattenhaus gezogen, tiefgrünes Blatt'
+    },
+    {
+        key: '13',
+        name: 'Jasmin Tee',
+        price_50g: 2.60,
+        price_100g: 4.60,
+        price_250g: 11.25,
+        type: 'China Grüntee',
+        description: 'Halbfermentierter Tee mit duftenden Jasmin Blüten'
+    },
+    {
+        key: '14',
+        name: 'Jasmin the Best',
+        price_50g: 3.55,
+        price_100g: 6.60,
+        price_250g: 16.25,
+        type: 'China Grüntee',
+        // description: ''
+    },
+    {
+        key: '15',
+        name: 'Jasmin Xian Yu (Ronnefeldt)',
+        // price_50g: ,
+        price_100g: 10.90,
+        price_250g: 27.25,
+        type: 'China Grüntee',
+        // description: ''
+    },
+    {
+        key: '16',
+        name: 'Pai Mu Tan BIO',
+        price_50g: 5.70,
+        price_100g: 10.90,
+        price_250g: 27.10,
+        type: 'China weißtee',
+        description: 'Feinste Blattspitzen, so gut wie Teerose'
+    },
+    {
+        key: '17',
+        name: 'China Silverneedle BIO',
+        price_50g: 12.35,
+        price_100g: 24.20,
+        price_250g: 60.25,
+        type: 'China weißtee',
+        description: 'Erste Grade, ein Blattsprossen-Tee der besten Qualität, von Hand Gepflückt, helle Tasse, zartes Aroma'
+    },
+    {
+        key: '18',
+        name: 'Weißer Mangotraum (Ronnefeldt)',
+        // price_50g: ,
+        price_100g: 7.60,
+        price_250g: 19.00,
+        type: 'China weißtee',
+        description: 'Pai Mu Tan mit Mango-Bergamotte-Geschmack, ein leichter und duftiger Tee, der wenig Teein enthält'
+    },
+    {
+        key: '19',
+        name: 'Formosa Pouchong',
+        price_50g: 9.90,
+        price_100g: 19.25,
+        price_250g: 48.10,
+        type: 'Formosa (Taiwan) Grüntee',
+        description: 'Ein sehr milder grüner Tee mit einer phantastischen süße, grüner Oolong'
+    },
+    {
+        key: '20',
+        name: 'Formosa Pi La Chun',
+        price_50g: 5.25,
+        price_100g: 10.00,
+        price_250g: 26.75,
+        type: 'Formosa (Taiwan) Grüntee',
+        description: 'Eine der großen Grüntee-Spezialitäten – aufwändig hergestelltes, feindrahtiges Blatt von blumiger Eleganz'
+    },
+    {
+        key: '21',
+        name: 'Formosa Fine Oolong',
+        price_50g: 2.75,
+        price_100g: 4.95,
+        price_250g: 12.35,
+        type: 'Formosa (Taiwan) Grüntee',
+        description: 'Zarter Oolong-Tee, hell in der Tasse, schwarzer Oolong'
+    },
+    {
+        key: '22',
+        name: 'TitKoon Yum',
+        price_50g: 2.85,
+        price_100g: 5.20,
+        price_250g: 12.75,
+        type: 'China Oolong-Tee (schwarzer Drachen, Halb fermentiert)',
+        description: 'Belebender Oolng-Tee, sehr würzig'
+    },
+    {
+        key: '23',
+        name: 'Kwai Flower',
+        price_50g: 3.20,
+        price_100g: 5.85,
+        price_250g: 14.35,
+        type: 'China Oolong-Tee (schwarzer Drachen, Halb fermentiert)',
+        description: 'Großblätriger China-Tee, dezenter Geschmack, erinnert an Pfirsich'
+    },
+    {
+        key: '24',
+        name: 'Orangenblüten Oolong',
+        price_50g: 3.00,
+        price_100g: 5.45,
+        price_250g: 13.35,
+        type: 'China Oolong-Tee (schwarzer Drachen, Halb fermentiert)',
+        // description: ''
+    },
+    {
+        key: '25',
+        name: 'Ceylon Watawalla Spezial FOP, BIO',
+        // price_50g: ,
+        price_100g: 9.35,
+        price_250g: 22.55,
+        type: 'Verschiedene Länder Grüntee (Ronnefeldt)',
+        // description: ''
+    },
+    {
+        key: '26',
+        name: 'Assam Khongea TGFOP',
+        // price_50g: ,
+        price_100g: 6.85,
+        price_250g: 17.10,
+        type: 'Verschiedene Länder Grüntee (Ronnefeldt)',
+        // description: ''
+    },
+    {
+        key: '27',
+        name: 'Darjeeling North Tukvar TGFOP, BIO',
+        // price_50g: ,
+        price_100g: 7.60,
+        price_250g: 19.00,
+        type: 'Verschiedene Länder Grüntee (Ronnefeldt)',
+        // description: ''
+    },
+    {
+        key: '28',
+        name: 'China Keemun Con',
+        // price_50g: ,
+        price_100g: 5.40,
+        price_250g: 13.50,
+        type: 'Verschiedene Länder Grüntee (Ronnefeldt)',
+        // description: ''
+    }
+]
+
+export default greenTeaData;
