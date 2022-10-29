dbeda2233a98f761478e9759f49237c71bae3f51
Mark Drecoll <markdrecoll@gmail.com>
2022-10-29 02:08:47 -0500

    Added Kräuterteemischungen page.

---
+++ src/teaData/kräutermischungenData.js
@@ -0,0 +1,204 @@
+const kräutermischungenData = [
+    {
+        key: "",
+        menuNumber: "K5",
+        name: "Afrikanischer Schönheitstee Bio",
+        price_50g: 3.10,
+        price_100g: 5.80,
+        price_250g: 14.25,
+        // type: "",
+        description: "Lemongras, Brombeerblätter, Fenchel, Rooibos, Verbenenkraut, Kamillenblüten, Apfelstücke, Zimtrinde, Ingwerstücke, Nelken, Kardamon, Natürliches Aroma"
+    },
+    {
+        key: "",
+        menuNumber: "K2",
+        name: "Entspann Dich Bio",
+        price_50g: 3.10,
+        price_100g: 5.80,
+        price_250g: 14.25,
+        // type: "",
+        description: "Mistelkraut, Brennesselblätter, Lemongras, Birkenblätter, Weißdornbeeren, Johanniskraut, natürliches Aroma"
+    },
+    {
+        key: "",
+        menuNumber: "K6",
+        name: "Fitness Kräutertee, Bio (Ronnefeldt)",
+        // price_50g: ,
+        price_100g: 5.80,
+        price_250g: 14.25,
+        // type: "",
+        description: "Rooibos, Lemongras, Verbene, Brombeer-, Melissenblätter, Rotsandelholz, Sonnenblumen-, Kornblumenblüten"
+    },
+    {
+        key: "",
+        menuNumber: "K9",
+        name: "Griechischer Bergtee /Sideritis)",
+        price_50g: 5.25,
+        price_100g: 10.00,
+        price_250g: 24.75,
+        // type: "",
+        description: "Geschnitten"
+    },
+    {
+        key: "",
+        menuNumber: "K10",
+        name: "Ingwer auf Kräuter (Ronnefeldt)",
+        // price_50g: ,
+        price_100g: 5.80,
+        price_250g: 14.25,
+        // type: "",
+        description: "Ingwer, Anis, Fenchel, Süßholz, Zitronengras, schwarze Pfefferkörner, Zimtstücken, Kamillienblüten. Süße Brombeerblätter, Rosenblüten, Zitronenverbene"
+    },
+    {
+        key: "",
+        menuNumber: "K13",
+        name: "Kräutertanz (ohne Aroma)",
+        price_50g: 3.10,
+        price_100g: 5.80,
+        price_250g: 14.25,
+        // type: "",
+        description: "Apfel-/Erdbeerstücke, Brombeer-/Brennesselblätter, Sultanas, Orangenschalen, Färberdisteln, Pfefferminze, Lemon-Myrte, Zitronenschalen, Blaukornblumenblüten, Ringelblumen- /Rosenblüten"
+    },
+    {
+        key: "",
+        menuNumber: "K29",
+        name: "Na Na Mint, Bio (Ronnefeldt)",
+        price_50g: 4.10,
+        price_100g: 7.70,
+        price_250g: 19.20,
+        // type: "",
+        description: "Ägyptischer, Kräutertee, aromatische Minze mit einem hohen Anteil ätherischer Öle"
+    },
+    {
+        key: "",
+        menuNumber: "K14",
+        name: "Reiki Kräuter (ohne Aroma)",
+        price_50g: 3.10,
+        price_100g: 5.80,
+        price_250g: 14.25,
+        // type: "",
+        description: "Der Begriff Reiki stammt von den japanischen Wörten rei (Kossmos) und KI (Lebensenegie). Apfelstücke, Mangowürfel, Zitronengras, Hagebutten-/Orangenschalen, Hibiskus-/Rosen-/Malvenblüten, Karottenraspeln, Brennesselblätter, süße Bromblätter, Mandarinen-Orangenfruchstücke, blu Kornblumenblüten, Rosenknospen, Färberdisteln"
+    },
+    {
+        key: "",
+        menuNumber: "K3",
+        name: "Schweizer Kräuter",
+        price_50g: 3.10,
+        price_100g: 5.80,
+        price_250g: 14.25,
+        // type: "",
+        description: "Apfel-/Karotten/Ingwerstücke, Orangenschale, Rooibos, Eibisch-/Pfefferminzblätter, Verbenen/Passionblumenkraut, Malvenblüten, natürliches Aroma"
+    },
+    {
+        key: "",
+        menuNumber: "K8",
+        name: "Stilltee",
+        price_50g: 2.20,
+        price_100g: 3.95,
+        price_250g: 9.60,
+        // type: "",
+        description: "Anis,Fenchel, Kümmel"
+    },
+    {
+        key: "",
+        menuNumber: "K4",
+        name: "Traumfigur Bio",
+        price_50g: 3.10,
+        price_100g: 5.80,
+        price_250g: 14.25,
+        // type: "",
+        description: "Krauseminze-Lemon-Geschmack, Apfelrstücke, Koriander, Nanaminze, Brennesselblätter, Orangenschalen, Lemongras, Zimtrinde, Ingwerstäbchen, Nelken, Kardamon, natürliches Aroma"
+    },
+    {
+        key: "",
+        // menuNumber: "",
+        name: "Pfefferminze geschnitten",
+        // price_50g: ,
+        price_100g: 5.00,
+        // price_250g: ,
+        // type: "",
+        // description: ""
+    },
+    {
+        key: "",
+        // menuNumber: "",
+        name: "Moringablätter",
+        // price_50g: ,
+        price_100g: 4.75,
+        // price_250g: ,
+        // type: "",
+        // description: ""
+    },
+    {
+        key: "",
+        // menuNumber: "",
+        name: "Fenchelsaat",
+        // price_50g: ,
+        price_100g: 3.55,
+        // price_250g: ,
+        // type: "",
+        // description: ""
+    },
+    {
+        key: "",
+        // menuNumber: "",
+        name: "Kamillenblüten",
+        // price_50g: ,
+        price_100g: 4.95,
+        // price_250g: ,
+        // type: "",
+        // description: ""
+    },
+    {
+        key: "",
+        // menuNumber: "",
+        name: "Salbeiblätter",
+        // price_50g: ,
+        price_100g: 4.10,
+        // price_250g: ,
+        // type: "",
+        // description: ""
+    },
+    {
+        key: "",
+        // menuNumber: "",
+        name: "Lemongrass",
+        // price_50g: ,
+        price_100g: 2.90,
+        // price_250g: ,
+        // type: "",
+        // description: ""
+    },
+    {
+        key: "",
+        // menuNumber: "",
+        name: "Hibiskusblüten",
+        // price_50g: ,
+        price_100g: 5.85,
+        // price_250g: ,
+        // type: "",
+        // description: ""
+    },
+    {
+        key: "",
+        // menuNumber: "",
+        name: "Brennnesselblätter",
+        // price_50g: ,
+        price_100g: 4.20,
+        // price_250g: ,
+        // type: "",
+        // description: ""
+    },
+    {
+        key: "",
+        // menuNumber: "",
+        name: "Cistus",
+        // price_50g: ,
+        price_100g: 3.95,
+        // price_250g: ,
+        // type: "",
+        // description: ""
+    }
+]
+
+export default kräutermischungenData;
