01d40b19bac471f14ebd08c72b23b808b44b27b8
mdre797 <markdrecoll@gmail.com>
2022-11-15 09:39:45 -0600

    Added image carousel captions, moved them to the top, added background to make them clearer.

--- src/components/ImageSlideShow.js
+++ src/components/ImageSlideShow.js
@@ -13,10 +13,10 @@ function ImageSlideShow() {
           src={process.env.PUBLIC_URL + "/assets/images/facebook_photos/teestube_facebook_photos(3).jpg"}
           alt="First slide"
         />
-        {/* <Carousel.Caption>
-          <h3>{t("Sample Text")}</h3>
-          <p>{t("You could have additional sample text here")}</p>
-        </Carousel.Caption> */}
+        <Carousel.Caption>
+          <h2 className="color-white">Willkommen in der Mainz<br />Teestube Teatime</h2>
+          {/* <p>{t("You could have additional sample text here")}</p> */}
+        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -24,10 +24,10 @@ function ImageSlideShow() {
           src={process.env.PUBLIC_URL + "/assets/images/facebook_photos/teestube_facebook_photos(1).jpg"}
           alt="Second slide"
         />
-        {/* <Carousel.Caption>
-          <h3>{t("Sample Text")}</h3>
-          <p>{t("You could have additional sample text here")}</p>
-        </Carousel.Caption> */}
+        <Carousel.Caption>
+          <h2 className="color-white">5% Rabatt jeden ersten Montag im Monat</h2>
+          {/* <p>{t("You could have additional sample text here")}</p> */}
+        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
